08fecdee2ccb80eafbdb38b28aa90cf7a3b2ef44
Sergio <sergio.qo000@gmail.com>
2019-02-17 22:48:53 +0100

    I add the project

---
+++ CHAT_MSDOS_CLIENTE/CHAT_MSDOS_CLIENTE/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CHAT_MSDOS_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* In this line is created an object Socket wich later is going to be 
+             * connected to localhost or the publilc ip of the network where we have 
+             * the server.
+             */
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            /* In this line is defined the ip wich is going to have the socket and the
+             * port wher it's going to work.
+             */
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+
+            /* Here we try to connect and if it can't connect returns "Error".
+             */
+            try
+            {
+                /* Here whe connect the socket to the before created address.
+                 */
+                client.Connect(ipep);
+                Console.WriteLine("Succesfully connected");
+
+                /* Here we create an string where we ar going to store the text we are
+                 * going to send.
+                 */
+                string data;
+
+                /* Here is delcared an array wich is going to
+                 * contain the message.
+                 */
+                byte[] send = new byte[100];
+
+                /* In this loop we ask for the message and save it in the array
+                 * to send it.
+                 */
+                do
+                {
+                    Console.Write("To send: ");
+                    data = Console.ReadLine();
+                    /* Here we convert the string in bytes using the 
+                     * GetBytes method.
+                     */
+                    send = Encoding.Default.GetBytes(data);
+
+                    // This line sends the array
+                    client.Send(send, 0, send.Length, 0);
+                } while (true);
+
+                client.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ CHAT_MSDOS_SERVIDOR/CHAT_MSDOS_SERVIDOR/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CHAT_MSDOS_SERVIDOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Here we create an address which is going to listen to any ip from
+             * this port. 
+             */
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 1234);
+
+            // Here we create a shocket
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // Here we bind the socket to de address.
+                sock.Bind(ipep);
+
+                //Here we put in listen the socket
+                sock.Listen(1);
+                Console.WriteLine("Listening...");
+
+                /* The program waits until the server is conneceted and it saves
+                 * the response in other socket.
+                 */
+                Socket client = sock.Accept();
+                Console.WriteLine("Succsefully Connected");
+
+                /* Here we have an string wich is where finaly is going to be stored
+                 * the message.
+                 */
+                string data;
+
+                /* In this array we save the array length we are going to receive
+                 * from the client to modify it of the server array later.
+                 */
+                int array_size;
+
+                //In this byte array we save the message bytes.
+                byte[] receive_info = new byte[255];
+
+                do
+                {
+                    //Here we store the length of the array in array_size.
+                    array_size = client.Receive(receive_info, 0, receive_info.Length, 0);
+                    
+                    //Here we resize the array of the server
+                    Array.Resize(ref receive_info, array_size);
+
+                    //Here we recibe the bytes and save it in the receive_info array.
+                    client.Receive(receive_info);
+
+                    //Here we convert the bytes of the array in an string
+                    data = Encoding.Default.GetString(receive_info);
+
+                    //Here we show the string
+                    Console.WriteLine(data);
+                } while (data != ".");
+
+                sock.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
